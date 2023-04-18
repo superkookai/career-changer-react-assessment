ba5cfee159a7b55e479b1128be84db654c4b3c5e
Weerawut Chaiyasomboon <weerawut@live.com>
2023-04-18 10:28:23 +0700

    Done for Owner Structure

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -1,10 +1,15 @@
+
 import Layout from "./Layout";
+import kai from "./kai.jpeg";
 
 const Owner = () => {
 
     return (
       <Layout>
-        <h1>Owner</h1>
+        <h1>56_Weerawut(Kai/ไก่) Group S </h1>
+        <img src={kai} style={{width:"200px"}}/>
+        <h3>Short Biography</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
       </Layout>
     )
   }
